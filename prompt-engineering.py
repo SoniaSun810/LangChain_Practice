a71cedbced16f759aa243cf1a7767483d0479cee
Fengnan <soniasun0925@gmail.com>
2023-08-23 11:19:19 -0700

    add refined prompts

---
+++ prompt-engineering.py
@@ -0,0 +1,41 @@
+system = """Based on the course name and description provided, please summarize the skill set necessary for an exemplary Teaching Assistant.  """
+format = """Your answer must follow Answer Format, without any other redundant words, TOKEN LIMIT 150:
+            - Expertise: [e.g., Data Science, Machine Learning]
+            - Programming Skills: [e.g., Python, Java]
+            - Libraries & Frameworks: [e.g., Numpy, Pandas]
+            - Tools: [e.g., Jupyter Notebook, PyCharm]
+            - Work Experience: [e.g., Junior Engineer, TA, Research Assistant]
+            - Projects Experience: [e.g., Full Stack Web Development, Mobile Application]
+            """
+example1 = """If given: ```
+            Course Name: Web Programming
+            Course Description: Exploration of the concepts, terminology, and popular frameworks for developing full-stack web applications. Students develop simple applications using multiple development stacks, and deploy them on the cloud.
+
+            Answer Example 1:
+            - Expertise: Full Stack Web Development
+            - Programming Skills: HTML5, CSS3, JavaScript, TypeScript
+            - Libraries & Frameworks: React.js, Express.js, Node.js
+            - Tools: Visual Studio Code, Git, Webpack, Chrome DevTools
+            - Work Experience: Front-end Developer at XYZ Corp, TA for Web Technologies course at ABC University
+            - Projects Experience: Developed a responsive website, full stack web development, contributed to an open-source project```
+            """
+example2 = """If given: ```
+            Course Name: Mobile Programming
+            Course Description: Exploration of contemporary libraries and frameworks for construction of mobile applications. Topics include emulators, mobile development standards and patterns, energy consumption issues, screen layout, among others.
+
+            Answer Example 2:
+            - Expertise: Mobile App Development
+            - Programming Skills: Java (for Android), Swift (for iOS)
+            - Libraries & Frameworks: React Native, Flutter, Android SDK, iOS SDK
+            - Tools: Android Studio, Xcode, Emulators, Mobile Device Debugging Tools
+            - Work Experience: Mobile App Developer at XYZ Tech, TA for a Computer Science course at ABC University
+            - Projects Experience: Developed a mobile app, contributed to a mobile project
+         ```
+            """
+question = """ Now given: ```
+            Course Name: GUI Programming
+            Course Description: Exploration of interactive software with substantial graphical user interface elements.  Topics include libraries and frameworks for GUI programming, layout design and alternatives, event-driven programming, among others. ```
+            Please give me your answer.
+            """
+query = system + example1 + example2 + question + format
+print(query)
